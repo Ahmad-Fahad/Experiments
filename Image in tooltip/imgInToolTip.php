a954e09f159f5cd1aa95f0133a2a9ca7d5e97f02
Ahmad-Fahad <ahmadsmfahad@gmail.com>
2018-05-04 01:35:04 +0600

    showing an image in a tooltip using html,css and php

---
+++ Image in tooltip/imgInToolTip.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<style>
+	.tooltip .tooltiptext {
+		background-image: url('<?php echo "img/tiger.jpg" ?>');
+	}
+</style>
+<body style="text-align:center;">
+
+<h2>Tooltip</h2>
+<p>Move the mouse over the text below:</p>
+
+<div class="tooltip">You are the Royal Bengal Tiger
+  <span class="tooltiptext"></span>
+</div>
+
+</body>
+</html>
